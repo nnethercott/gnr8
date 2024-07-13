8676bcf3c8986b4e3da39e469688200219196eb7
nnethercott <nathaniel@deepomatic.com>
2024-07-13 20:29:20 +0200

    inside one big thread

--- src/stream.rs
+++ src/stream.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3_tch::PyTensor;
 use std::io::{self, Write};
 use tch::{self, kind::Kind, Cuda, Device, IndexOp, Tensor};
-use pyo3::types::PyDict;
 
 use crate::gen::{len, GenerationConfig, PartialGenerate};
 
@@ -25,47 +25,47 @@ where
         input_ids: &Tensor,
         mut gc: GenerationConfig,
     ) -> Result<Tensor> {
-        let device = input_ids.device();
-        let mut input_ids = input_ids.shallow_clone().to(device); //&Tensor to Tensor
-        let init_len = len!(input_ids);
-        let mut done;
+        Python::with_gil(|py| {
+            let device = input_ids.device();
+            let mut input_ids = input_ids.shallow_clone().to(device); //&Tensor to Tensor
+            let init_len = len!(input_ids);
+            let mut done;
 
-        let tokenizer = gc.streamer.take().unwrap().tokenizer;
+            let tokenizer = gc.streamer.take().unwrap().tokenizer;
 
-        while len!(input_ids) - init_len <= gc.max_new_tokens as i64 {
-            (done, input_ids) = match T::partial_generate(model, input_ids, &gc) {
-                Ok(res) => res,
-                Err(e) => {
-                  println!("{:?}", e);
-                  panic!();
+            while len!(input_ids) - init_len <= gc.max_new_tokens as i64 {
+                (done, input_ids) = match T::partial_generate(model, input_ids, &gc) {
+                    Ok(res) => res,
+                    Err(e) => {
+                        println!("{:?}", e);
+                        panic!();
+                    }
+                };
+                //decode
+                let kwargs = PyDict::new(py);
+                let _ = kwargs.set_item("skip_special_tokens", true);
+
+                let s = tokenizer
+                    .call_method("decode", (PyTensor(input_ids.i(0).copy()),), Some(&kwargs))
+                    .unwrap();
+                s.str().unwrap().to_string_lossy().into_owned();
+
+                let s = tokenizer
+                    .call_method("decode", (PyTensor(input_ids.i(0).copy()),), None)
+                    .unwrap();
+                let decoded = s.str().unwrap().to_string_lossy().into_owned();
+
+                print!("\x1B[2J\x1B[1;1H");
+                print!("{:?}", decoded);
+                io::stdout().flush().unwrap();
+
+                if done {
+                    break;
                 }
-            };
-            //decode
-            //let decoded = Python::with_gil(|py|{
-            //    let kwargs = PyDict::new(py);
-            //    let _ = kwargs.set_item("skip_special_tokens", true);
-            //    
-            //    let s = tokenizer
-            //        .call_method("decode", (PyTensor(input_ids.i(0).copy()),), Some(&kwargs))
-            //        .unwrap();
-            //    s.str().unwrap().to_string_lossy().into_owned();
-            //
-            //});
-            let s = tokenizer
-                .call_method("decode", (PyTensor(input_ids.i(0).copy()),), None)
-                .unwrap();
-            let decoded = s.str().unwrap().to_string_lossy().into_owned();
-
-            print!("\x1B[2J\x1B[1;1H");
-            print!("{:?}", decoded);
-            io::stdout().flush().unwrap();
-
-            if done {
-                break;
             }
-        }
-        println!("");
+            println!("");
 
-        Ok(input_ids)
+            Ok(input_ids)
+        })
     }
 }
--- test.py
+++ test.py
@@ -20,10 +20,11 @@ tok = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 tinyllama = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 model = M(tinyllama).to(device)
 
-prompt = "tell me a story about a cat"
-input_ids = tok(prompt, return_tensors = 'pt')['input_ids'].to(device)
+messages = [{"role": "user", "content": "tell me a story about a cat."}]
+prompt = tok.apply_chat_template(messages, tokenizer=False)
+input_ids = torch.tensor(prompt).unsqueeze(0).to(device)
 
 kwargs = {"tokenizer": tok}
-#res = generate(model, input_ids, kwargs)
+res = generate(model, input_ids, kwargs)
 
 
